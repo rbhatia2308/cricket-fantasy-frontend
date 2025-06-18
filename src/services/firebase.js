e37fe1c778035921c7d4520bafd1632550e4a0ca
Rajeev Bhatia <rajeev.bhatia@Rajeevs-MacBook-Air-2.local>
2025-06-18 21:43:33 +0530

    Added required files and fixed build issues

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,14 +1,21 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "your_firebase_api_key",
-  authDomain: "your_firebase_auth_domain",
-  projectId: "your_project_id",
+  apiKey: "AIzaSyAx1nH7WF75AfjK4HjIVqYtF1o6Alfyf8A",
+  authDomain: "hohacricket.firebaseapp.com",
+  projectId: "hohacricket",
+  storageBucket: "hohacricket.firebasestorage.app",
+  messagingSenderId: "77987625287",
+  appId: "1:77987625287:web:6260ae5541f25ab37c3462",
+  measurementId: "G-NF0GPFLZ7H"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const provider = new GoogleAuthProvider();
-
-export const signInWithGoogle = () => signInWithPopup(auth, provider);
+const analytics = getAnalytics(app);
