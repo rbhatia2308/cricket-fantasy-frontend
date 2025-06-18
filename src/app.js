8d4d5ddb7cb35b6b4d438ed8ecd7ca6af1bd0b15
Rajeev Bhatia <rajeev.bhatia@Rajeevs-MacBook-Air-2.local>
2025-06-18 21:40:05 +0530

    Added browserlist for compatibility

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Cricket Fantasy</h1>
+      <p>Login to get started!</p>
+    </div>
+  );
+}
+
+export default App;
