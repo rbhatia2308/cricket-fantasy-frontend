149f73dabba35cd47869c246d5910ea995244a6f
Rajeev Bhatia <rajeev.bhatia@Rajeevs-MacBook-Air-2.local>
2025-06-19 13:08:27 +0530

    Complete MatchList UI enhancements

---
+++ src/utils/teamNameMap.js
@@ -0,0 +1,23 @@
+const teamNameMap = {
+    IND: "india",
+    ENG: "england",
+    AUS: "australia",
+    SA: "south-africa",
+    PAK: "pakistan",
+    NZ: "new-zealand",
+    BAN: "bangladesh",
+    SL: "sri-lanka",
+    WI: "west-indies",
+    AFG: "afghanistan",
+    IRE: "ireland",
+    ZIM: "zimbabwe",
+    UAE: "uae",
+    SCO: "scotland",
+    NAM: "namibia",
+    NEP: "nepal",
+    USA: "usa",
+    NED: "netherlands",
+  };
+  
+  export default teamNameMap;
+  
