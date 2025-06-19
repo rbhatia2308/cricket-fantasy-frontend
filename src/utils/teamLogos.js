23b53cd3c1f2e7aa1224db51a947383b14bb56d3
Rajeev Bhatia <rajeev.bhatia@Rajeevs-MacBook-Air-2.local>
2025-06-19 11:12:53 +0530

    Add team logos

---
+++ src/utils/teamLogos.js
@@ -0,0 +1,10 @@
+const teamLogos = {
+    "India": "https://example.com/logos/india.png",
+    "Australia": "https://example.com/logos/australia.png",
+    "Pakistan": "https://example.com/logos/pakistan.png",
+    "England": "https://example.com/logos/england.png",
+    // Add all other teams similarly
+  };
+  
+  export default teamLogos;
+  
